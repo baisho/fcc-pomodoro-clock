9b6753f2dec56e7cb69fb57982cb116f9a7403b3
Bánk <mienkapalya7@gmail.com>
2018-05-09 15:23:49 +0200

    working forward in class

--- pomodoro-js/script.js
+++ pomodoro-js/script.js
@@ -2,8 +2,13 @@
 var sessionLength = 25;
 var breakLength = 5;
 var loopThis = 0;
-var start = 0;
+//var start = 0;
 var paused = true;
+var repeatMyInterval = 0;
+//var countDownTime = 0;
+var stoppedValue = 0;
+var clickCounter = 0;
+
 
 // Adding 1 to session time.
 // By clicking.
@@ -14,11 +19,8 @@ document.getElementById("sessionPlus").addEventListener("mousedown", function ()
     loopThis = setInterval(adding1ToSession, 200);
 });
 
-document.getElementsByTagName("body")[0].addEventListener("mouseup", function () {
-    clearInterval(loopThis);
-});
-
 function adding1ToSession() {
+    clickCounter = 0;
     sessionLength += 1;
     inject();
 }
@@ -34,6 +36,7 @@ document.getElementById("sessionMinus").addEventListener("mousedown", function (
 });
 
 function substracting1FromSession() {
+    clickCounter = 0;
     sessionLength -= 1;
     if (sessionLength <= 1) {
         sessionLength = 1;
@@ -52,6 +55,7 @@ document.getElementById("breakPlus").addEventListener("mousedown", function () {
 });
 
 function adding1ToBreak() {
+    clickCounter = 0;
     breakLength += 1;
     inject();
 }
@@ -67,12 +71,18 @@ document.getElementById("breakMinus").addEventListener("mousedown", function () 
 });
 
 function substracting1FromBreak() {
+    clickCounter = 0;
     breakLength -= 1;
     if (breakLength <= 1) {
         breakLength = 1;
     }
     inject();
 }
+
+// Stop changing sessionLength or breakLength when holding of the mouse is finished.
+document.getElementsByTagName("body")[0].addEventListener("mouseup", function () {
+    clearInterval(loopThis);
+});
 
 // Function to refresh the set times.
 function inject() {
@@ -88,55 +98,100 @@ inject();
 
 
 // Starting the session by clicking on time.
-document.getElementById("sessionValue").addEventListener("click", showTime); 
+document.getElementById("sessionValue").addEventListener("click", showTime);
 
 function showTime() {
-    
+    clickCounter ++;
+    if (clickCounter > 2) {
+        sessionLength = ;
+    }
+    /*if (clickCounter % 2 == 1) {
+
+    } else if (clickCounter % 2 == 0) {
+        
+    }*/
+
+    console.log("clickCounter: " + clickCounter);
     if (paused) {
+
+        // Get starting date and time
+        var start = Date.now();
+
+        // Set the date we're counting down to
+        var countDownTime = (start + sessionLength * 60 * 1000);
+
+        // Update the count down every 1 second
+        repeatMyInterval = setInterval(repeatIt, 1000);
         paused = false;
+
     } else if (!paused) {
+        stopIt();
+                document.getElementById("demo").innerHTML = "Stopped";
         paused = true;
     }
+    function ez() { console.log(paused); }
+    setTimeout(ez, 1500);
+}
 
-    console.log(paused);
-start = Date.now();//currentTime();
-// Get todays date and time
+function repeatIt() {
 
-// Set the date we're counting down to
-var countDownTime = (start + sessionLength * 60 * 1000);
+        now = Date.now();
 
-// Update the count down every 1 second
-var x = setInterval(function () {
-    
-    now = Date.now();
+        // Find the distance between start an the count down date
+        var distance = countDownTime - now;
+        //var distance = stoppedValue - now;
 
-    // Find the distance between start an the count down date
-    var distance = countDownTime - now;
+        // Time calculations for hours, minutes and seconds
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    // Time calculations for hours, minutes and seconds
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-    if (seconds < 10) {
-        seconds = "0" + seconds;
-    }
-
-    // Display the result in the element with id="sessionValue"
-    if (distance > 60*60*1000) {
-        if (minutes < 10) {
-            minutes = "0" + minutes;
+        if (seconds < 10) {
+            seconds = "0" + seconds;
         }
-    document.getElementById("sessionValue").innerHTML = hours + ":"
-        + minutes + ":" + seconds;
-    } else {
-        document.getElementById("sessionValue").innerHTML = minutes + ":" + seconds;
+
+        // Display the result in the element with id="sessionValue"
+        if (distance > 60 * 60 * 1000) {
+            if (minutes < 10) {
+                minutes = "0" + minutes;
+            }
+            document.getElementById("sessionValue").innerHTML = hours + ":"
+                + minutes + ":" + seconds;
+        } else {
+            document.getElementById("sessionValue").innerHTML = minutes + ":" + seconds;
+        }
+
+        // If the count down is finished, write some text 
+        if (distance <= 0) {
+            stopIt();
+            document.getElementById("demo").innerHTML = "EXPIRED";
+        }
     }
 
-    // If the count down is finished, write some text 
-    if (distance < 0) {
-        clearInterval(x);
-        document.getElementById("demo").innerHTML = "EXPIRED";
-    }
-}, 1000);
-};
+function stopIt() {
+    var atStopping = Date.now();
+    console.log("countDownTime: " + countDownTime);
+    console.log("stoppedValue: " + stoppedValue);
+    clearInterval(repeatMyInterval);
+    return atStopping;
+}
+
+function timeTransformer(timeInMiliSeconds) {
+    var hours = Math.floor((timeInMiliSeconds % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((timeInMiliSeconds % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((timeInMiliSeconds % (1000 * 60)) / 1000);
+    return hours, minutes, seconds;
+}
+
+
+/*var myVar = setInterval(function(){ myTimer() }, 1000);
+
+function myTimer() {
+    var d = new Date();
+    var t = d.toLocaleTimeString();
+    document.getElementById("demo").innerHTML = t;
+}
+
+function myStopFunction() {
+    clearInterval(myVar);
+}*/
