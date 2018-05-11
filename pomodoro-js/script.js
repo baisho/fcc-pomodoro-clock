ffdf9a94e5a255c892c503d67873cf2cf7d3d7f3
Bánk <mienkapalya7@gmail.com>
2018-05-11 15:49:10 +0200

    Sound is working, can be turned on and off.

--- pomodoro-js/script.js
+++ pomodoro-js/script.js
@@ -2,8 +2,8 @@
 var session = true;
 var clickCounter = 0;
 var sessionButtonClickCounter = 0;
-var sessionLength = 25;
-var breakLength = 5;
+var sessionLength = 0.2;
+var breakLength = 0.2;
 var sectionLength = 0;
 var sectionName = "";
 var loopThis = 0;
@@ -12,6 +12,7 @@ var currentTime1Val = 0;
 var currentTime2Val = 0;
 var endTimeVal = 0;
 var rememberTimeLeftVal = 0;
+var volumeUp = true;
 
 
 // Adding 1 to session time
@@ -32,7 +33,7 @@ function adding1ToSession() {
         // Everytime the countdown is stopped
         else if (clickCounter % 2 == 0) {
             clickCounter = 0;
-            sessionLength ++;
+            sessionLength++;
             sessionButtonClickCounter++;
             injectSessionLength();
             injectSectionLength();
@@ -63,7 +64,7 @@ function substracting1FromSession() {
         else if (clickCounter % 2 == 0) {
             clickCounter = 0;
             sessionButtonClickCounter++;
-            sessionLength --;
+            sessionLength--;
             // sessionLength cannot be less then one
             if (sessionLength <= 1) {
                 sessionLength = 1;
@@ -102,7 +103,7 @@ function adding1ToBreak() {
                 clickCounter = 0;
             }
         }
-        breakLength ++;
+        breakLength++;
         injectBreakLength();
         if (!session) {
             clickCounter = 0;
@@ -136,7 +137,7 @@ function substracting1FromBreak() {
                 clickCounter = 0;
             }
         }
-        breakLength --;
+        breakLength--;
         // breakLength cannot be less then one
         if (breakLength <= 1) {
             breakLength = 1;
@@ -219,24 +220,31 @@ function showTime() {
     injectSectionName();
     clickCounter++;
 
-    // Every odd counts
+    // Every odd counts: start or continue countdown
     if (clickCounter % 2 == 1) {
         sessionOrBreak();
         currentTime1();
         endTime();
-        // Update the count down every 0.1 second by setInterval
+        var audio5sec = document.getElementById('5sec');
+        if (!audio5sec.paused || audio5sec.currentTime) {
+            play5sec();
+        }
+        // Update the countdown every 0.1 second by setInterval
         repeatMyInterval = setInterval(countDown, 100);
     }
-    // Every even counts
+    // Every even counts: pause countdown
     else if (clickCounter % 2 == 0) {
         sessionOrBreak();
         currentTime2();
         rememberTimeLeft();
+        var audio5sec = document.getElementById('5sec');
+        if (!audio5sec.paused || audio5sec.currentTime) {
+            pause5sec();
+        }
         // Stop the countdown
         stopIt();
     }
 }
-
 
 // Function for running in every seconds, thus making a countdown
 function countDown() {
@@ -268,7 +276,7 @@ function countDown() {
         document.getElementById("sectionValue").innerHTML = minutes + ":" + seconds;
     }
 
-    // If the count down is finished, stop the current one and change for the next session
+    // If the countdown is finished, stop the current one and change for the next session
     if (distance <= 100) {
         sessionButtonClickCounter = 0;
         stopIt();
@@ -281,6 +289,10 @@ function countDown() {
         clickCounter = 0;
         showTime();
         return;
+    }
+
+    if (distance <= 6100) {
+        play5sec();
     }
 }
 
@@ -301,4 +313,31 @@ function sessionOrBreak() {
 // Function for stopping the countdown by clearInterval
 function stopIt() {
     clearInterval(repeatMyInterval);
+}
+
+// Function to play 5sec.mp3 audio file
+function play5sec() {
+    document.getElementById("5sec").play();
+}
+
+// Function to pause 5sec.mp3 audio file
+function pause5sec() {
+    document.getElementById("5sec").pause();
+}
+
+document.getElementById("volumeSymbol").addEventListener("click", toggleSoundOnAndOff);
+
+function toggleSoundOnAndOff() {
+
+    if (volumeUp) {
+        document.getElementById("volumeUp").classList.add("hidden");
+        document.getElementById("volumeOff").classList.remove("hidden");
+        document.getElementById("5sec").muted = true;
+        volumeUp = false;
+    } else if (!volumeUp) {
+        document.getElementById("volumeUp").classList.remove("hidden");
+        document.getElementById("volumeOff").classList.add("hidden");
+        document.getElementById("5sec").muted = false;
+        volumeUp = true;
+    }
 }
